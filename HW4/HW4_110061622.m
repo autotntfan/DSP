2ac2c53fa6c96b150beb3e70870bef622eafa98a
autotntfan <jam310157@gmail.com>
2021-11-08 08:59:52 +0800

    finished HW4

--- HW4/HW4_110061622.m
+++ HW4/HW4_110061622.m
@@ -35,8 +35,8 @@ xlabel('frequency (Hz)')
 ylabel('degree/s')
 
 % part 2 variables
-v = 10;     % velocity [m/s]
-T = 0.0001; % sampling interval [s]
+v = 10;      % velocity [m/s]
+T = 0.0001;  % sampling interval [s]
 t = 10;t = 0:T:t-T; % time axis
 y = v*t;   
 figure
@@ -48,6 +48,7 @@ g = (D*sin(2*pi*y/L) + abs(D*sin(2*pi*y/L))) ./2;
 plot(t,g,'r')
 legend('g(y)','|g(y)|','(g(y)+|g(y)|)/2')
 x1 = g;
+% initial condition for x2
 x2 = [0 0];
 alpha = T*b + 2*m;
 for ii = 3:length(g)
@@ -62,7 +63,7 @@ legend('x1','x2')
 xlabel('time (s)')
 ylabel('displacement (m)')
 MaxDisp = max(x2)
-
+% another way to determine x2
 y = filter([T*b 2*T^2*k -T*b],[T*b+2*m 2*T^2*k-4*m 2*m-T*b],x1,[]);
 figure
 plot(t,x1)
@@ -79,16 +80,18 @@ plot(freq,abs(G))
 title('spectrum of g')
 xlabel('frequency (Hz)')
 ylabel('Mag')
-
+xlim([-20 20])
+% set sampling freq
 fs = 2e1;
 t = 0:1/fs:10;
 h = (487*sqrt(111)*sin(9*sqrt(111)/16.*t)+1665*cos(9*sqrt(111)/16.*t)).*exp(-15/16.*t)/888;
 figure
 plot(t,h)
 xlabel('time (s)')
-ylabel('amplitude')
+ylabel('amp')
 title('h(t)')
 figure
+% use freqz to draw response
 freqz(h,fs,length(t),fs)
 [amp,w] = freqz(h,fs,length(t),fs);
 figure
@@ -97,9 +100,10 @@ title('amplitude response')
 xlabel('frequency (rad/s)')
 ylabel('amplitude (dB)')
 
-H = fftshift(fft(h));
-freq = linspace(-fs/2,fs/2,length(h));
-figure
-plot(freq,abs(H))
-xlabel('freq (Hz)')
-xlim([-5 5])
+% draw the spectrum to check bandwidth
+% H = fftshift(fft(h));
+% freq = linspace(-fs/2,fs/2,length(h));
+% figure
+% plot(freq,abs(H))
+% xlabel('freq (Hz)')
+% xlim([-5 5])
